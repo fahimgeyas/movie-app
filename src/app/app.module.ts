1f8ac1a4d951f33d690f97cb36155dc6981ad8ad
fahimgeyas <fahimgeyas@gmail.com>
2022-07-25 13:17:19 +0530

    Added complete home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+
+interface home{
+  id: number,
+  movieName: string,
+  rating: number;
+  posterLink: string,
+  isBollywood: string
+}
+
+interface detail{
+  id:number,
+  name:string,
+  details:string,
+  runtime:number,
+  director:string
+}
 
 @Component({
   selector: 'app-home',
@@ -7,9 +25,110 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('draggable')
+  private draggableElement!: ElementRef;
+  display=false
+
+  constructor(private http:HttpClient){}
+
+  url = ["http://localhost:8080/weekly-top-movies","http://localhost:8080/weekly-top-bollywood-movies"]
+  detailsUrl=""
+
+  top: home[]=[]
+  bollywood: home[]=[]
+  detials: detail[]=[]
+
+
+  weeklyTop = [
+    {
+      title: 'Captain Marvel',
+      img: '../../assets/captainMarvel.jpg',
+      rating: '7.1'
+    },
+    {
+      title: 'Bohemian Rhapsody',
+      img: '../../assets/bohemianRhapsody.jpg',
+      rating: '8.1'
+    },
+    {
+      title: 'Green Book',
+      img: '../../assets/greenBook.jpg',
+      rating: '8.3'
+    },
+    {
+      title: 'A Star is Born',
+      img: '../../assets/aStarIsBorn.jpg',
+      rating: '7.8'
+    },
+
+  ];
+
+  weeklyTopBollywood = [
+    {
+      title: 'Kalank',
+      img: '../../assets/kalank.jpg',
+      rating: '7.1'
+    },
+    {
+      title: 'Bohemian Rhapsody',
+      img: '../../assets/bohemianRhapsody.jpg',
+      rating: '7.1'
+    },
+    {
+      title: 'RAW',
+      img: '../../assets/RAW.jpg',
+      rating: '7.1'
+    },
+    {
+      title: 'Total Dhamaal',
+      img: '../../assets/totalDhamaal.jpg',
+      rating: '7.1'
+    },
+
+  ];
+
+  trailers = [
+    {
+      title: 'Marvel Studios\' Avengers: Endgame - Official Trailer',
+      img: '../../assets/endgame.jpg',
+      rating: '7.1'
+    },
+    {
+      title: 'Disney\'s Aladdin - Official Trailer: In Theaters May 24!',
+      img: '../../assets/aladdin.jpeg',
+      rating: '7.1'
+    },
+    {
+      title: 'Game of Thrones | Season 8 | Official Trailer (HBO)',
+      img: '../../assets/GoT.jpg',
+      rating: '7.1'
+    },
+    {
+      title: 'SPIDER-MAN: NO WAY HOME - Official Trailer (HD)',
+      img: '../../assets/spider-man.jpg',
+      rating: '7.1'
+    },
+
+  ];
 
   ngOnInit(): void {
+
+    this.http.get<home[]>(this.url[0],{responseType:"json"}).subscribe((res) =>{
+      this.top=res;
+    })
+    this.http.get<home[]>(this.url[1],{responseType:"json"}).subscribe((res) =>{
+      this.bollywood=res;
+    })
   }
+
+  hide(name:string){
+    this.draggableElement.nativeElement.remove();
+    this.display=true
+    this.detailsUrl="http://localhost:8080/get-movie-detail/"+name
+    this.http.get<home[]>(this.detailsUrl,{responseType:"json"}).subscribe((res) =>{
+      this.bollywood=res;
+    })
+  }
+  
 
 }
